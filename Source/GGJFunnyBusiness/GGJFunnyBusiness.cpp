25e78ef29c67faab72406885461c6b1cf7a55b5d
Sebastian Zapart <259383@stud.inn.no>
2024-01-26 21:06:42 +0100

    GitInit

---
+++ Source/GGJFunnyBusiness/GGJFunnyBusiness.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GGJFunnyBusiness.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJFunnyBusiness, "GGJFunnyBusiness" );
